6392c24248dafcfb09e52fc2647d817574bc4f25
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2025-12-26 10:17:44 -0500

    force yaml structure to have values

---
+++ param_templates/nml_to_yaml.py
@@ -0,0 +1,21 @@
+import f90nml
+import yaml
+
+# Read Fortran namelist
+nml = f90nml.read('input.nml')
+
+# Transform to add "values" wrapper
+def wrap_in_values(data):
+    """Recursively wrap leaf values in {'values': value} structure."""
+    if isinstance(data, dict):
+        return {key: wrap_in_values(val) for key, val in data.items()}
+    else:
+        # Leaf node - wrap it
+        return {'values': data}
+
+# Apply transformation
+wrapped_nml = wrap_in_values(nml.todict())
+
+# Write to YAML
+with open('output.yaml', 'w') as f:
+    yaml.dump(wrapped_nml, f, default_flow_style=False)
